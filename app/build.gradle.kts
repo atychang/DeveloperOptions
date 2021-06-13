37e57e7048963914a28cd198a56e096de2d3edd8
Allen Chang <allen.ty.chang@gmail.com>
2021-06-13 15:51:15 +0800

    Add signing configs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+
 plugins {
     id("com.android.application")
     kotlin("android")
@@ -15,11 +17,21 @@ android {
         versionName = "1.0"
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file(gradleLocalProperties(rootDir).getProperty("keystorePath"))
+            storePassword = gradleLocalProperties(rootDir).getProperty("storePass")
+            keyAlias = gradleLocalProperties(rootDir).getProperty("keystoreAlias")
+            keyPassword = gradleLocalProperties(rootDir).getProperty("keyPass")
+        }
+    }
+
     buildTypes {
         named("release") {
             isShrinkResources = true
             isMinifyEnabled = true
             proguardFiles("proguard-android-optimize.txt", "proguard-rules.pro")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 
